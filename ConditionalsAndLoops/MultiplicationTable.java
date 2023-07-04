f0c18fb319bf2f4212d8d62a80a219a498fbbd2c
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-04 23:53:38 +0530

    done

---
+++ ConditionalsAndLoops/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package ConditionalAndLoops;
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args){
+     Scanner s = new Scanner(System.in);
+     int n = s.nextInt();
+     for(int i = 1 ; i < 11 ; i++){
+        int a = n*i;
+        System.out.println(a);
+     }
+    }
+}
