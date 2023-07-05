1f0c660682c9822a7f95182fdac311812fbbc59e
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-05 23:26:30 +0530

    Update MultiplicationTable.java

--- ConditionalsAndLoops/MultiplicationTable.java
+++ ConditionalsAndLoops/MultiplicationTable.java
@@ -1,6 +1,7 @@
 package ConditionalAndLoops;
 
 import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args){
      Scanner s = new Scanner(System.in);
