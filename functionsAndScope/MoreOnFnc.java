7b59e83925e0ed384234f7f40d6305ecf24a3249
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-08-02 22:04:15 +0530

    Add files via upload

---
+++ functionsAndScope/MoreOnFnc.java
@@ -0,0 +1,31 @@
+package FunctionsAndScope;
+import java.util.Scanner;
+public class MoreOnFnc {
+    // Sum of two numbers
+
+    public static int Sum(int a  , int b){
+        int c  = a + b ;
+        return c;
+    }
+    // public static void main(String[]args){
+    //  Scanner s  = new Scanner(System.in);
+    //  int t = s.nextInt();
+    //  int d  = s.nextInt();
+    //  int td = Sum(t , d);
+    //  System.out.print(td);
+    // }
+
+    // print Even numbers
+    public static void Even(){
+        for(int i = 1 ; i <= 100 ; i++){
+         if(i%2 == 0 ){
+            System.out.print(i);
+         }
+        }
+    }
+    public static void main(String[] args){
+       Even();
+        
+    }
+
+}
